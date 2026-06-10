f6763d274227819ef49892ed8747b14437285195
abhi2003vaish <abhivaish2003@gmail.com>
2026-06-10 23:49:13 +0530

    11.3(Redis Cache in Spring boot)

--- src/main/java/com/example/Caching_Application/config/CacheConfig.java
+++ src/main/java/com/example/Caching_Application/config/CacheConfig.java
@@ -1,10 +1,39 @@
 package com.example.Caching_Application.config;
 
+import com.example.Caching_Application.dto.EmployeeDto;
+import com.example.Caching_Application.entities.Employee;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.*;
+import tools.jackson.databind.ObjectMapper;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching  // without this annotation caching will remain disabled and we can use caching related annotation
 public class CacheConfig {
+
+//    configure RedisCacheMananger
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+
+
+        RedisCacheConfiguration redisCacheConfiguration=RedisCacheConfiguration.defaultCacheConfig()
+                .prefixCacheNameWith("my-redis-")      // add prefix to all cache names in redis cache
+                .entryTtl(Duration.ofSeconds(60))       // data Time to live in redis cache is 1 min
+                .enableTimeToIdle()  //this line reset the expiration time or TTL if it get accessed before TTL expires
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new JacksonJsonRedisSerializer<EmployeeDto>(EmployeeDto.class)))
+                ;
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
 
 }
--- src/main/java/com/example/Caching_Application/dto/EmployeeDto.java
+++ src/main/java/com/example/Caching_Application/dto/EmployeeDto.java
@@ -2,8 +2,10 @@ package com.example.Caching_Application.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class EmployeeDto {
+public class EmployeeDto implements Serializable {
     private Long id;
     private String email;
     private String name;
